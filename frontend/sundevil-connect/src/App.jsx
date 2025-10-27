8a270af770a6e2cf20e8b3370f50798ad64d038c
Andrew Rath <arath5@gmail.com>
2025-10-27 15:18:58 -0700

    Add a basic frontend

---
+++ frontend/sundevil-connect/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p>Testing!</p>
+      <h1>TEST HEADER</h1>
+    </>
+  )
+}
+
+export default App
