f77b62c8171f7af91e1de68578daaa77beed01c6
ssammarshall <ssammarshall@gmail.com>
2025-10-31 23:15:41 -0700

    Created basic Club and Event serializers and set them in Club and Event view sets respectively. API routes no longer throw exceptions.

--- backend/sundevilconnect/serializers.py
+++ backend/sundevilconnect/serializers.py
@@ -1,3 +1,22 @@
 from rest_framework import serializers
 from .models import Club, Event
 
+class ClubSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Club
+        fields = ['id', 'name', 'description']
+
+
+# Removes description field.
+class ClubSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Club
+        fields = ['id', 'name']
+
+
+class EventSerializer(serializers.ModelSerializer):
+    club = ClubSimpleSerializer()
+
+    class Meta:
+        model = Event
+        fields = ['id', 'name', 'date', 'location', 'is_paid_event', 'attendees', 'max_num_of_attendees', 'club']
--- backend/sundevilconnect/views.py
+++ backend/sundevilconnect/views.py
@@ -1,9 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Club, Event
+from .serializers import ClubSerializer, EventSerializer
 
 class ClubViewSet(ModelViewSet):
     queryset = Club.objects.all()
+    serializer_class = ClubSerializer
+
 
 class EventViewSet(ModelViewSet):
     queryset = Event.objects.all()
+    serializer_class = EventSerializer
