36c46e959b70e7a6a1ad5b8dbd5f35f791429328
ssammarshall <ssammarshall@gmail.com>
2025-11-11 12:29:02 -0700

    Updated Makefile, .bat and .ps1 files to include copying the backend .env.example and creating a .env

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
 
 CALL npm i
+copy backend\.env.example backend\.env
 CALL python -m pip install -r backend\requirements.txt
 CALL python backend\manage.py migrate
--- build.ps1
+++ build.ps1
@@ -2,6 +2,7 @@ $ErrorActionPreference = 'Stop'
 
 npm i
 Set-Location .\backend
+Copy-Item .env.example .env
 python -m pip install -r requirements.txt
 python manage.py migrate
 Set-Location ..
