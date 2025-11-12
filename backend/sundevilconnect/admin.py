5bbfe4ae14469e078168ad20df8c83e89cbce92e
ssammarshall <ssammarshall@gmail.com>
2025-11-12 13:08:05 -0700

    Admin panels for users, memberships, clubs and events

--- backend/sundevilconnect/admin.py
+++ backend/sundevilconnect/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, Membership, Club, Event
 
-# Register your models here.
+admin.site.register(Membership)
+admin.site.register(Club)
+admin.site.register(Event)
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ("username", "email", "first_name", "last_name", "is_staff")
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2"),
+            },
+        ),
+    )
