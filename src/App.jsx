5bceb82fe6bbf861a4d8112a9b8a2f75acbb5675
legowiifun <arath5@gmail.com>
2025-11-13 11:21:17 -0700

    Completely refactor logic for determining what page to draw

--- src/App.jsx
+++ src/App.jsx
@@ -18,22 +18,36 @@ function App() {
   const [activePage, setActivePage] = useState(pages.mainPage);
   const [activeRole, setActiveRole] = useState(roles.unknown);
   const [id, setId] = useState(-1);
-  //There is likely a way to refactor this that is much better
-  return (
-    <>
-      {activePage!==pages.loginPage?<LoginBtn setPage={(page) => setActivePage(page)} role={activeRole}></LoginBtn>:<></>}
-      {activePage!==pages.mainPage?<MainPageBtn setPage={()=>setActivePage(pages.mainPage)}></MainPageBtn>:<></>}
-      {
-        activePage===pages.mainPage?<MainScreen setPage={(page) => setActivePage(page)} role={activeRole}></MainScreen>:
-        (activePage===pages.loginPage?<LoginPage setPage={(page)=>setActivePage(page)} setRole={(role) => setActiveRole(role)}></LoginPage>:
-        (activePage===pages.clubsPage?<ClubsScreen role={activeRole}></ClubsScreen>:
-        (activePage===pages.eventsPage?<EventsScreen role={activeRole}></EventsScreen>:
-        (activePage===pages.newUserPage?<NewUserPage setRole={(role) => setActiveRole(role)} setPage={(page)=>setActivePage(page)}></NewUserPage>:
-        (activePage===pages.profilePage?<ProfilePage role={activeRole}></ProfilePage>:
-          (<></>))))))
-      }
-    </>
-  )
+  
+  //set up the page
+  let topButtons = (<div>
+    {activePage!==pages.loginPage?<LoginBtn setPage={(page) => setActivePage(page)} role={activeRole}></LoginBtn>:<></>}
+    {activePage!==pages.mainPage?<MainPageBtn setPage={()=>setActivePage(pages.mainPage)}></MainPageBtn>:<></>}
+  </div>);
+  let page;
+  switch(activePage) {
+    case pages.loginPage:
+      page=<LoginPage setPage={(page)=>setActivePage(page)} setRole={(role) => setActiveRole(role)}></LoginPage>;
+      break;
+    case pages.clubsPage:
+      page=<ClubsScreen role={activeRole}></ClubsScreen>;
+      break;
+    case pages.eventsPage:
+      page=<EventsScreen role={activeRole}></EventsScreen>;
+      break;
+    case pages.newUserPage:
+      page=<NewUserPage setRole={(role) => setActiveRole(role)} setPage={(page)=>setActivePage(page)}></NewUserPage>;
+      break;
+    case pages.profilePage:
+      page=<ProfilePage role={activeRole}></ProfilePage>;
+      break;
+    default:
+      page=<MainScreen setPage={(page) => setActivePage(page)} role={activeRole}></MainScreen>;
+  }
+  return (<>
+    {topButtons}
+    {page}
+  </>)
 }
 
 export default App
