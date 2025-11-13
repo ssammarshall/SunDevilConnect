5115f4ecf2791c5cce0ac80ae2c8870ff4a99faa
ssammarshall <ssammarshall@gmail.com>
2025-11-13 13:39:15 -0700

    IsClubAdmin and IsClubMember permission classes

---
+++ backend/sundevilconnect/permissions.py
@@ -0,0 +1,24 @@
+from rest_framework.permissions import BasePermission
+
+from .models import Membership
+
+class IsClubAdmin(BasePermission):
+    def has_permission(self, request, view):
+        user = request.user
+        if not user or not user.is_authenticated: return False
+        
+        club_id = request.data.get('club')
+        if club_id is None: return False # No club provided.
+
+        return Membership.objects.filter(user=user, club_id=club_id, role=Membership.ROLE_CHOICES[Membership.LEADER]).exists()
+
+
+class IsClubMember(BasePermission):
+    def has_permission(self, request, view):
+        user = request.user
+        if not user or not user.is_authenticated: return False
+        
+        club_id = request.data.get('club')
+        if club_id is None: return False # No club provided.
+
+        return Membership.objects.filter(user=user, club_id=club_id).exists()
