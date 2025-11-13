7fe6be497c6b8c9fdec7ab0ea9a43a2f2c376dcd
ssammarshall <ssammarshall@gmail.com>
2025-11-13 13:47:04 -0700

    Add IsClubLeaderOrReadOnly and IsClubMemberOrReadOnly

--- backend/sundevilconnect/permissions.py
+++ backend/sundevilconnect/permissions.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 from .models import Membership
 
@@ -13,6 +13,12 @@ class IsClubLeader(BasePermission):
         return Membership.objects.filter(user=user, club_id=club_id, role=Membership.ROLE_CHOICES[Membership.LEADER]).exists()
 
 
+class IsClubLeaderOrReadyOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS: return True
+        return IsClubLeader.has_permission(self, request, view)
+
+
 class IsClubMember(BasePermission):
     def has_permission(self, request, view):
         user = request.user
@@ -22,3 +28,9 @@ class IsClubMember(BasePermission):
         if club_id is None: return False # No club provided.
 
         return Membership.objects.filter(user=user, club_id=club_id).exists()
+
+
+class IsClubMemberOrReadyOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS: return True
+        return IsClubMember.has_permission(self, request, view)
