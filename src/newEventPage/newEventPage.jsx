df0164fd432a4396d6e68157d705debf7a61bd48
legowiifun <arath5@gmail.com>
2025-11-14 15:28:12 -0700

    Add fields to new event and update event pages

--- src/newEventPage/newEventPage.jsx
+++ src/newEventPage/newEventPage.jsx
@@ -1,8 +1,18 @@
 import { pages } from '../Pages';
 
 function NewEventPage({id, setPage}) {
+    function submitButton() {
+        //make requests to add an event
+        //when done, call this
+        setPage(pages.mainPage);
+    }
     return (<>
-        
+        <input id="name" placeholder="name of the event"></input><br/><br/>
+        <input id="date" type="datetime-local"></input><br/><br/>
+        <input id="location" placeholder="location of the event"></input><br/><br/>
+        Is this event paid?: <input id="isPaid" type="checkbox"></input><br/><br/>
+        <input id="maxAttendees" type="number" placeholder="number of attendees"></input><br/><br/>
+        <button onClick={submitButton}>Submit!</button>
     </>)
 }
 export default NewEventPage;
