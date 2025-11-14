59c1df26da30e0ec38905ddc8b399580a77ca6e1
legowiifun <arath5@gmail.com>
2025-11-14 15:34:08 -0700

    Add structure for updating and creating posts
    I still have not added the post requests for these. We can add those next week.

--- src/newPostPage/newPostPage.jsx
+++ src/newPostPage/newPostPage.jsx
@@ -1,6 +1,14 @@
 import { pages } from '../Pages';
 
 function NewPostPage({id, setPage}) {
-    return (<></>)
+    function onButtonClick() {
+        //do post request to reach main clubs page
+        setPage(pages.mainPage);
+    }
+    return (<>
+        <input id="title" placeholder="title"></input><br/><br/>
+        <textarea id="body" placeholder="body"></textarea><br/><br/>
+        <button id="submit" onClick={onButtonClick}>submit</button>
+    </>)
 }
 export default NewPostPage;
