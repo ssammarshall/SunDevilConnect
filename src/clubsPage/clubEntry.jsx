fdfb99df22d1e17e145e31641ebe908ee2fe6255
legowiifun <arath5@gmail.com>
2025-11-20 14:30:49 -0700

    Early version of joining clubs.

--- src/clubsPage/clubEntry.jsx
+++ src/clubsPage/clubEntry.jsx
@@ -1,5 +1,6 @@
 import { roles } from "../roles";
 import { pages } from "../Pages";
+import { authenticate } from "../utils";
 function ClubEntry({club, role, setPage, setId, clubMemberships, setClubMemberships}) {
     function buttonClick() {
         setId(club.id);
@@ -8,9 +9,38 @@ function ClubEntry({club, role, setPage, setId, clubMemberships, setClubMembersh
 
     function clubJoin() {
         //do the requests needed to add a user to the club
+        let body = {};
+        body.id=club.id;
+        //body.role=roles.requestedToJoin;
         let membership = clubMemberships;
-        membership[membership.length]={club_id:club.id,club_name:club.name,role:"M"};
-        buttonClick();
+        membership[membership.length]={club_id:club.id,club_name:club.name,role:roles.requestedToJoin};
+        authenticate().then((data) => {
+            if (!data.access) {
+                console.log("No access token returned from refresh");
+                return;
+            }
+            //use the new token to access what you want
+            fetch(process.env.REACT_APP_API_URL + "/connect/clubs/"+club.id+"/join/?format=json", {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                    Authorization: "JWT " + data.access,
+                },
+                body: JSON.stringify(body)
+            })
+            //after fetching the data, print it out
+            .then((resp) => {
+                //console.log(resp);
+                return resp.json();
+            })
+            .then(function (data) {
+                console.log(data);
+            })
+            .catch(function (error) {
+                console.log("error: " + error);
+            });
+        });
     }
 
     //different return values based on the role
