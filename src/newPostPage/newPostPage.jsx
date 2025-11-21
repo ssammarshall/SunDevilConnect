31a17b2eaf87470bb959adbdf7b0b189a37ec968
legowiifun <arath5@gmail.com>
2025-11-21 14:51:17 -0700

    Add endpoint connection for adding a new post

--- src/newPostPage/newPostPage.jsx
+++ src/newPostPage/newPostPage.jsx
@@ -1,8 +1,38 @@
 import { pages } from '../Pages';
+import { authenticate } from '../utils';
 
 function NewPostPage({id, setPage}) {
     function onButtonClick() {
-        //do post request to reach main clubs page
+        let body={};
+        body.title=document.getElementById("title").value;
+        body.body=document.getElementById("body").value;
+        authenticate().then((data) => {
+            if (!data.access) {
+                console.log("No access token returned from refresh");
+                return;
+            }
+            //use the new token to access what you want
+            fetch(process.env.REACT_APP_API_URL + "/connect/clubs/"+id+"/content/?format=json", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                    Authorization: "JWT " + data.access,
+                },
+                body: JSON.stringify(body)
+            })
+            //after fetching the data, print it out
+            .then((resp) => {
+                //console.log(resp);
+                return resp.json();
+            })
+            .then(function (data) {
+                console.log(data);
+            })
+            .catch(function (error) {
+                console.log("error: " + error);
+            });
+        });
         setPage(pages.mainPage);
     }
     return (<>
